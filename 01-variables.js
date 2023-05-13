c92d9ad5938dff0c737f433cdf23ced523c33e64
BogdanAlixandrescu <bogdan.alixandrescu@yahoo.com>
2023-05-13 23:17:12 +0200

    create varianles session 01

---
+++ 01-variables.js
@@ -0,0 +1,26 @@
+var greetings = 'hola desde'
+var school = 'pmba'
+
+var greetingsFromSchool = greetings + ' ' + school;
+
+console.log(greetingsFromSchool);
+
+
+// holaQueTal (camelCase)
+// FINAL_TEXT (PASCLA_CASE)
+
+// String
+// Number
+// Boolean (true-false)
+// Array
+// Object
+
+
+var ageAsString = '30'
+var ageAsNumber = 30 
+
+var arrayOfValues = [30, '30', false, true ,4]
+var objectOfValues = {
+    age: 30,
+    ageAsNumber: ageAsNumber
+}
